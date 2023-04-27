5f94cdd07380e754e80c9a440b076ebd6146da2b
manuelrosa <manuel98rosa@gmail.com>
2023-04-27 19:41:01 -0300

    Intento 2 de comit

--- src/proyecto_algoritmos/ProyectoAlgoritmos.java
+++ src/proyecto_algoritmos/ProyectoAlgoritmos.java
@@ -4,7 +4,11 @@ public class ProyectoAlgoritmos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Soy gonza y cambie el texto intento 220");
+		System.out.println("Soy Manu y pushee en el intento 2");
 	}
 
 }
+
+
+
+//asdasdasd
